0aea7daf49d3d622371e968e69ad625fd477b180
Klypalskyi <klypalskyi@gmail.com>
2019-10-09 02:30:39 +0300

    Updates

---
+++ precache-manifest.f48b1fe07e24a7d70af2a87bcd02b2a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bca65d19591fcbb752843901b560f2c3",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "91f0740e08ebc5d3ef79",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.c711bfe3.chunk.css"
+  },
+  {
+    "revision": "a1b5bd7687fbe255f6ce",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.917a5c9b.chunk.js"
+  },
+  {
+    "revision": "91f0740e08ebc5d3ef79",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.4096234a.chunk.js"
+  },
+  {
+    "revision": "fc9ab068169c9aa7a3f2",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime~main.216778b9.js"
+  }
+]);
